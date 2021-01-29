62bd2620349df9e7a9b70f62fba9b525a333d202
Tristan Kaffel <tristan.kaffel@gmail.com>
2021-01-29 15:36:30 +0100

    route Inscription + connexion bdd

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const uid2 = require("uid2");
+const SHA256 = require('crypto-js/sha256');
+const encBase64 = require('crypto-js/enc-base64');
+var mongoose = require('mongoose');
+
+const userModel = require('../models/user');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,3 +14,208 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+router.post('/sign-up/student', async function (req, res, next) {
+
+  console.log('PASSING', req.body);
+
+  var error = []
+  var result = false
+  var saveUser = null
+  var token = null
+
+  const data = await userModel.findOne({
+    email: req.body.emailFromFront
+  })
+
+  if (data != null) {
+    error.push('User Already Exist')
+  }
+
+  if (req.body.firstNameFromFront == ''
+    || req.body.lastNameFromFront == ''
+    || req.body.emailFromFront == ''
+    || req.body.passwordFromFront == ''
+  ) {
+    console.log('ERROR');
+    error.push('Empty Field')
+  }
+
+  if (error.length === 0) {
+
+    console.log('NO ERROR');
+
+    var salt = uid2(32)
+    var newUser = new userModel({
+      firstName: req.body.firstNameFromFront,
+      lastName: req.body.lastNameFromFront,
+      email: req.body.emailFromFront,
+      grade: req.body.gradeFromFront,
+      sex: req.body.sexFromFront,
+      password: SHA256(req.body.passwordFromFront + salt).toString(encBase64),
+      token: uid2(32),
+      salt: salt,
+      // A MODIFIER
+      role: "Student",
+      
+
+    })
+    console.log('Student', req.body.firstNameFromFront)
+
+    saveUser = await newUser.save()
+
+
+    if (saveUser) {
+      result = true
+      token = saveUser.token
+    }
+  }
+  res.json({ result, saveUser, error, token })
+})
+
+router.post('/sign-up/parent', async function (req, res, next) {
+
+  console.log('PASSING', req.body);
+
+  var error = []
+  var result = false
+  var saveUser = null
+  var token = null
+
+  const data = await userModel.findOne({
+    email: req.body.emailFromFront
+  })
+
+  if (data != null) {
+    error.push('User Already Exist')
+  }
+
+  if (req.body.firstNameFromFront == ''
+    || req.body.lastNameFromFront == ''
+    || req.body.emailFromFront == ''
+    || req.body.passwordFromFront == ''
+  ) {
+    console.log('ERROR');
+    error.push('Empty Field')
+  }
+
+  if (error.length === 0) {
+
+    console.log('NO ERROR');
+
+    var salt = uid2(32)
+    var newUser = new userModel({
+      firstName: req.body.firstNameFromFront,
+      lastName: req.body.lastNameFromFront,
+      email: req.body.emailFromFront,
+      // CIVILITE ??
+      sex: req.body.sexFromFront,
+      password: SHA256(req.body.passwordFromFront + salt).toString(encBase64),
+      token: uid2(32),
+      salt: salt,
+      // A MODIFIER
+      role: "Parent",
+      
+
+    })
+    console.log('Student', req.body.firstNameFromFront)
+
+    saveUser = await newUser.save()
+
+
+    if (saveUser) {
+      result = true
+      token = saveUser.token
+    }
+  }
+  res.json({ result, saveUser, error, token })
+})
+
+router.post('/sign-in/student', async function (req, res, next) {
+
+
+  var result = false
+  var user = null
+  var error = []
+  var token = null
+
+  if (req.body.emailFromFront == ''
+    || req.body.passwordFromFront == ''
+  ) {
+    error.push('Empty Field')
+  }
+
+  if (error.length == 0) {
+    user = await userModel.findOne({
+      email: req.body.emailFromFront,
+    })
+
+    console.log("log-user", user)
+
+    if (user) {
+      const passwordEncrypt = SHA256(req.body.passwordFromFront + user.salt).toString(encBase64)
+
+      if (passwordEncrypt == user.password) {
+        result = true
+        token = user.token
+      } else {
+        result = false
+        error.push('Incorrect Password ')
+      }
+
+    } else {
+      error.push('Incorrect Email')
+    }
+  }
+
+  console.log("SIGNIN SUCESS", user, result)
+
+  res.json({ result, user, error, token })
+
+
+})
+
+router.post('/sign-in/parent', async function (req, res, next) {
+
+
+  var result = false
+  var user = null
+  var error = []
+  var token = null
+
+  if (req.body.emailFromFront == ''
+    || req.body.passwordFromFront == ''
+  ) {
+    error.push('Empty Field')
+  }
+
+  if (error.length == 0) {
+    user = await userModel.findOne({
+      email: req.body.emailFromFront,
+    })
+
+    console.log("log-user", user)
+
+    if (user) {
+      const passwordEncrypt = SHA256(req.body.passwordFromFront + user.salt).toString(encBase64)
+
+      if (passwordEncrypt == user.password) {
+        result = true
+        token = user.token
+      } else {
+        result = false
+        error.push('Incorrect Password ')
+      }
+
+    } else {
+      error.push('Incorrect Email')
+    }
+  }
+
+  console.log("SIGNIN SUCESS", user, result)
+
+  res.json({ result, user, error, token })
+
+
+})
+
